07ba00a48ac3c0c3600dfd6f68413b7e00fd298e
Dean Cadry <dean.cadry@reddamhouse.com.au>
2024-02-25 12:26:29 +1100

    hsitory .html working

--- History/history.js
+++ History/history.js
@@ -1,7 +1,7 @@
-
+// Purpose: To display the last 3 calculations of each tax type
 function displayCT(){
 
-        var last3 = JSON.parse(localStorage.getItem("ctLast3"));
+        let last3 = JSON.parse(localStorage.getItem("ctLast3"));
         document.getElementById("header").innerHTML = "Company Tax";
         document.getElementById("1").innerHTML = "Most Recent Calculation: $" + last3[0];
         document.getElementById("2").innerHTML = "2nd Most Recent Calculation: $" + last3[1];
@@ -11,9 +11,8 @@ function displayCT(){
 }
 
 function displayPT() {
-
-        var last3 = JSON.parse(localStorage.getItem("ptLast3"));
-        document.getElemmentById("header").innerHTML = "Payroll Tax";
+        let last3 = JSON.parse(localStorage.getItem("ptLast3"));
+        document.getElementById("header").innerHTML = "Payroll Tax";
         document.getElementById("1").innerHTML = "Most Recent Calculation: $" + last3[0];
         document.getElementById("2").innerHTML = "2nd Most Recent Calculation: $" + last3[1];
         document.getElementById("3").innerHTML = "3rd Most Recent Calculation: $" + last3[2];
@@ -21,8 +20,7 @@ function displayPT() {
 }
 
 function displayIT() {
-    
-        var last3 = JSON.parse(localStorage.getItem("itLast3"));
+        let last3 = JSON.parse(localStorage.getItem("itLast3"));
         document.getElementById("header").innerHTML = "Income Tax";
         document.getElementById("1").innerHTML = "Most Recent Calculation: $" + last3[0];
         document.getElementById("2").innerHTML = "2nd Most Recent Calculation: $" + last3[1];
