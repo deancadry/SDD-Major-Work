2d1842fd33a3c29c114418c8d637a54ce4bc47b7
Dean Cadry <dean.cadry@reddamhouse.com.au>
2024-02-29 10:29:08 +1100

    ^^ working...

--- js/signup_page.js
+++ js/signup_page.js
@@ -21,91 +21,94 @@ function test() {
 
 
 
-// Check if username has more than 4 letters
-if (inputUsername.length <= 3) {
+  // Check if username has more than 4 letters
+  if (inputUsername.length <= 3) {
     alert("Username must have 4 or more letters");
     window.location = "../Login-Signup-System/signup_page.html"; // Redirect to back to sign up page
     unameValid = false;
     return false;
 
-}
+  };
 
-// Check if password meets the criteria
- if (inputPassword.length <= 8 || /\s/.test(inputPassword) || !/\d/.test(inputPassword) || !/[A-Z]/.test(inputPassword)){
+  // Check if password meets the criteria
+  if (inputPassword.length <= 8 || /\s/.test(inputPassword) || !/\d/.test(inputPassword) || !/[A-Z]/.test(inputPassword)){
      alert("Password must be longer than 8 characters, contain no spaces, have at least one number and one capitalized letter");
      window.location = "../Login-Signup-System/signup_page.html"; // Redirect to back to sign up page
      pwordValid = false;
      return false;
 
- }
+  };
 
-// Check if PhoneNum is meets the cirtieria 
- if ((/\s/).test(inputPhone) || /-/.test(inputPhone)) { 
+  // Check if PhoneNum is meets the cirtieria 
+  if ((/\s/).test(inputPhone) || /-/.test(inputPhone)) { 
      alert("Phone Number must be 10 numbers, no spaces or dashes");
     window.location = "../Login-Signup-System/signup_page.html"; // Redirect to back to sign up page
      phoneValid = false;
      return false;
-}
+  };
 
- //Check if Email is empty
- if (inputEmail.trim() === "" || !inputEmail.includes("@") || !inputEmail.includes(".")) {
+
+  //Check if Email is empty
+  if (inputEmail.trim() === "" || !inputEmail.includes("@") || !inputEmail.includes(".")) {
     alert("Invalid email address");
      window.location = "../Login-Signup-System/signup_page.html"; // Redirect to back to sign up page
      emailValid = false;
      return false;
- }
+  };
 
 
-if (unameValid != false && pwordValid != false && phoneValid != false && emailValid != false){ // are all the booleans === true? 
+  if (unameValid != false && pwordValid != false && phoneValid != false && emailValid != false){ // are all the booleans === true? 
      alert("Your user ID is " + inputUserId + " please keep this number safe");
-     push();
+     push(username, password, phone, email, userId, inputUsername, inputPassword, inputPhone, inputEmail, inputUserId);
+
      alert("You have successfully signed up");
      window.location = "../Login-Signup-System/login_page.html"; // Redirect to home page
 
   return true;
 
-}
+  };
 
 
-
-
-if ( unameValid == false || pwordValid == false || phoneValid == false || emailValid == false) { // are the booleans === false
+  if ( unameValid == false || pwordValid == false || phoneValid == false || emailValid == false) { // are the booleans === false
 alert("Login failed");
 window.location = "../Login-Signup-System/signup_page.html"; // Redirect to sign up page
 return false;
-}
-}
+  }
+};
 
-function push(username, password, phone, email, userId, inputUsername, inputPassword, inputPhone, inputEmail, inputUserId){
+function push(username, password, phone, email, userId, inputUsername, inputPassword, inputPhone, inputEmail, inputUserId) {
+  // console.log(username + password + phone + email + userId + inputUsername + inputPassword + inputPhone + inputEmail + inputUserId)
 
-if (username.length === password.length === phone.length === email.length === userId.length){
+  if (username.length == password.length == phone.length == email.length == userId.length){
 
-  alert("Adding values to array and local storage");
-  // .push() adds the new user details to the end of the array
+    alert("Adding values to array and local storage");
 
-  username.push(inputUsername);
-  password.push(inputPassword);
-  phone.push(inputPhone);
-  email.push(inputEmail);
-  userId.push(inputUserId);
-  // .setItem() sets the new user details to the local storage
+    
+    // .push() adds the new user details to the end of the array
+
+    username.push(inputUsername);
+    password.push(inputPassword);
+    phone.push(inputPhone);
+    email.push(inputEmail);
+    userId.push(inputUserId);
+    // .setItem() sets the new user details to the local storage
 
 
-  localStorage.setItem("username", JSON.stringify(username));
-  localStorage.setItem("password", JSON.stringify(password));
-  localStorage.setItem("phone", JSON.stringify(phone));
-  localStorage.setItem("email", JSON.stringify(email));
-  localStorage.setItem("userId", JSON.stringify(userId)); 
-  alert("finished");
-  return true;
+    localStorage.setItem("username", JSON.stringify(username));
+    localStorage.setItem("password", JSON.stringify(password));
+    localStorage.setItem("phone", JSON.stringify(phone));
+    localStorage.setItem("email", JSON.stringify(email));
+    localStorage.setItem("userId", JSON.stringify(userId)); 
+    alert("finished");
+    return true;
 
-}
-else {
+  }
+  else {
 
-  alert("An error has occured. Please try again. If the issue continues please contact me at dean.cadry@reddamhouse.com.au");
-  window.location = "../Login-Signup-System/signup_page.html"; // Redirect to sign up page
-  return false;
-}
-}
+    alert("An error has occured. Please try again. If the issue continues please contact me at dean.cadry@reddamhouse.com.au");
+    window.location = "../Login-Signup-System/signup_page.html"; // Redirect to sign up page
+    return false;
+};
+};
 
 
